222562c1692819275fe08247f04bc620e64a6b5d
Pablo Santana <pablo.santana116@alu.ulpgc.es>
2024-03-01 13:15:57 +0000

    WIP: Creating dynamic table

--- templates/table-template.js
+++ templates/table-template.js
@@ -1,18 +1,33 @@
-let rowTemplate = document.getElementById('row-template');
+let rowTemplate = document.getElementById('th-template');
+let headerTemplate = document.querySelector('#table-header template');
+let table = document.getElementById('table');
 
-createRow();
-createRow();
-console.log(rowTemplate);
+createTableHeaders("ID");
+createTableHeaders("Name");
+createRow(["Content 1", "Content 2"]); // Example row with content
+createRow(["Content A", "Content B"]); // Another example row with content
 
-function createRow() {
-    var i = 0;
-    let newRow = rowTemplate.cloneNode(true);
-    let columns = newRow.getElementsByTagName("td");
+function createTableHeaders(text) {
+    let newHeaderRow = document.importNode(headerTemplate.content, true);
+    let headerColumns = newHeaderRow.querySelectorAll("th");
 
-    for (const column of columns) {
-        column.firstElementChild.innerHTML = "Columna " + i;
-        i++;
-    }
+    headerColumns.forEach((headerColumn, index) => {
+        headerColumn.textContent = text;
+    });
 
-    table.appendChild(newRow);
+    table.querySelector("thead").appendChild(newHeaderRow);
+}
+
+function createRow(contents) {
+    let newRow = rowTemplate.firstElementChild.content;
+    let column = newRow.cloneNode(true);
+
+    contents.forEach((content) => {
+        const element = column.clone();
+        element.firstElementChild.innerHTML = content;
+        rowTemplate.appendChild(element);
+    });
+
+    // Append the new row to the tbody
+    table.querySelector("tbody").appendChild(newRow);
 }
