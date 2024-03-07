8338ebbf348fd8f984cef7d846c21ec3a58a7f82
david-koschel <113340344+david-koschel@users.noreply.github.com>
2024-03-07 11:42:37 +0000

    WIP

--- templates/basic-templates/include-html.js
+++ templates/basic-templates/include-html.js
@@ -1,22 +1,9 @@
 async function includeHTML() {
     for (const element of document.querySelectorAll("[html-page]")) {
-        const response = await fetch(element.getAttribute("html-page"));
-        if (response.ok) {
-            element.innerHTML = await response.text();
-            if (element.getAttribute("home-buttons")) {
-                homeButtons(element.getAttribute("home-buttons").split(','));
-            }
-        }
+        fetch(element.getAttribute("html-page"))
+            .then(response => response.ok ? response.text() : "CONTENT COULD NOT BE LOADED")
+            .then((text) => element.innerHTML = text);
     }
-}
-
-function homeButtons(buttons) {
-    let temp = document.getElementById('home-buttons');
-    let content = temp.firstElementChild.content;
-    buttons.forEach(buttonName => createButton(buttonName))
-    function createButton(text) {
-        const node = content.cloneNode(true);
-        node.firstElementChild.innerHTML = text;
-        temp.appendChild(node);
+    for (const element of document.querySelectorAll("[home-buttons]")) {
     }
 }
