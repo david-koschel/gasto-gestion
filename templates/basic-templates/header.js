c881c40e160a9045fe2b55cdb9d98e20749a9033
david-koschel <113340344+david-koschel@users.noreply.github.com>
2024-03-07 13:13:42 +0000

    Updated footer and header templates

--- templates/basic-templates/header.js
+++
@@ -1,9 +0,0 @@
-function homeButtons(buttons) {
-    let parent = document.getElementById('home-buttons');
-    let template = parent.firstElementChild.content;
-    buttons.forEach(buttonName => {
-        const node = template.cloneNode(true);
-        node.firstElementChild.innerHTML = buttonName;
-        parent.appendChild(node);
-    })
-}
--- templates/basic-templates/include-html.js
+++ templates/basic-templates/include-html.js
@@ -2,8 +2,22 @@ async function includeHTML() {
     for (const element of document.querySelectorAll("[html-page]")) {
         fetch(element.getAttribute("html-page"))
             .then(response => response.ok ? response.text() : "CONTENT COULD NOT BE LOADED")
-            .then((text) => element.innerHTML = text);
+            .then((text) => {
+                element.innerHTML = text;
+                executeScripts(element);
+            });
     }
-    for (const element of document.querySelectorAll("[home-buttons]")) {
-    }
+}
+
+function executeScripts(container) {
+    const scripts = container.querySelectorAll('script');
+    scripts.forEach(script => {
+        const scriptTag = document.createElement('script');
+        if (script.src) {
+            scriptTag.src = script.src;
+        } else {
+            scriptTag.textContent = script.textContent;
+        }
+        document.head.appendChild(scriptTag).parentNode.removeChild(scriptTag);
+    });
 }
