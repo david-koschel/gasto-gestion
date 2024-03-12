c5b1920089b15267de986d74af4956fd99a39b77
david-koschel <113340344+david-koschel@users.noreply.github.com>
2024-03-12 12:52:05 +0000

    Added json fetch to header

--- templates/basic-templates/header/header.js
+++ templates/basic-templates/header/header.js
@@ -1,9 +1,20 @@
-const buttons = ["lorem ipsum", "lorem ipsum", "lorem ipsum"]
-
 const parent = document.getElementById('home-buttons');
 const template = parent.firstElementChild.content;
-buttons.forEach(buttonName => {
-    const node = template.cloneNode(true);
-    node.firstElementChild.innerHTML = buttonName;
-    parent.appendChild(node);
-})
+const script = document.getElementById('header-script')
+
+loadButtonData();
+
+function loadButtonData() {
+    fetch("/data.json")
+        .then(result => result.json())
+        .then(json => access(script.getAttribute("json-data"), json).forEach(buttonName => {
+                const node = template.cloneNode(true);
+                node.firstElementChild.innerHTML = buttonName;
+                parent.appendChild(node);
+            })
+        );
+}
+
+const access = (path, object) => {
+    return path.split('.').reduce((o, i) => o[i], object)
+}
--- templates/basic-templates/include-html.js
+++ templates/basic-templates/include-html.js
@@ -6,20 +6,21 @@ function includeHTML() {
             .then(response => response.ok ? response.text() : "CONTENT COULD NOT BE LOADED")
             .then((text) => {
                 element.innerHTML = text;
-                executeScripts(element);
+                executeScripts(element, element.getAttribute("json-data"));
             });
     }
 }
 
-function executeScripts(container) {
+function executeScripts(container, json) {
     const scripts = container.querySelectorAll('script');
-    scripts.forEach(script => {
+    scripts.forEach((script) => {
         const scriptTag = document.createElement('script');
-        if (script.src) {
-            scriptTag.src = script.src;
-        } else {
-            scriptTag.textContent = script.textContent;
-        }
-        document.head.appendChild(scriptTag).parentNode.removeChild(scriptTag);
+        if (json) scriptTag.setAttribute("json-data", json);
+        if (script.id) scriptTag.id = script.id;
+        if (script.src) scriptTag.src = script.src;
+        else scriptTag.textContent = script.textContent;
+
+        document.head.appendChild(scriptTag);
+        script.remove()
     });
 }
