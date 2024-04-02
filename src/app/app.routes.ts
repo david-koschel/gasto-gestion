618c8f4acc332e9e41c1aacc283ead207db5b9cf
Pablo Santana <pablo.santana116@alu.ulpgc.es>
2024-04-02 11:55:34 +0100

    Updated cookies and privacy policy to angular components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {CookiesPolicyComponent} from "./web-pages/cookies-policy/cookies-policy.component";
+import {PrivacyPolicyComponent} from "./web-pages/privacy-policy/privacy-policy.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "cookies",
+    component: CookiesPolicyComponent
+  },
+  {
+    path: "politica-privacidad",
+    component: PrivacyPolicyComponent
+  }
+];
