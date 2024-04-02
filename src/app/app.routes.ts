fde320ba52ab271fcf3a149fc0ab9b64226a6553
Jaiches22 <jaimecalzada22@gmail.com>
2024-04-02 12:24:04 +0100

    Starting angular user-types

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { UserTypeComponent } from './components/user-type/user-type.component';
 
-export const routes: Routes = [{path: "home", component: HomeComponent}];
+export const routes: Routes = [
+    {path: "home", component: HomeComponent},
+    {path: "user-type", component: UserTypeComponent}
+];
---
+++ src/app/components/user-type/user-type.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-type',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-type.component.html',
+  styleUrl: './user-type.component.scss'
+})
+export class UserTypeComponent {
+
+}
