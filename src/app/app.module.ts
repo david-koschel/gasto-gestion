1afcf9ba4c600e9de07299ad868990ed0103b1bf
Pablo Santana <pablo.santana116@alu.ulpgc.es>
2024-05-03 11:32:03 +0100

    Added firebase file storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {provideFirebaseApp} from "@angular/fire/app";
 import {environment} from "../environments/environment";
 import {getFirestore, provideFirestore} from "@angular/fire/firestore";
 import firebase from "firebase/compat";
+import { provideStorage, getStorage } from "@angular/fire/storage"
 import initializeApp = firebase.initializeApp;
 import {AppComponent} from "./app.component";
 
@@ -13,6 +14,7 @@ import {AppComponent} from "./app.component";
     CommonModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
     AppComponent
   ]
 })
