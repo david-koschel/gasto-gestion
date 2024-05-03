deb6a5efbb9c827133a79940fc024884b6ede57d
Pablo Santana <pablo.santana116@alu.ulpgc.es>
2024-05-03 18:15:42 +0100

    Updated app config and deleted app module

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,37 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { InMemoryScrollingOptions, provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from "@angular/common/http";
+import {provideFirebaseApp} from "@angular/fire/app";
+import {getFirestore, provideFirestore} from "@angular/fire/firestore";
+import firebase from "firebase/compat";
+import initializeApp = firebase.initializeApp;
 
 const scrollConfig: InMemoryScrollingOptions = {
   scrollPositionRestoration: 'top',
   anchorScrolling: 'enabled',
 };
 
+const firebaseConfig = {
+  apiKey: "AIzaSyC8yq3hTHPM-a7qxrw8FRm6ISqSl6qhnaw",
+  authDomain: "gasto-gestion.firebaseapp.com",
+  databaseURL: "https://gasto-gestion-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "gasto-gestion",
+  storageBucket: "gasto-gestion.appspot.com",
+  messagingSenderId: "728351854859",
+  appId: "1:728351854859:web:f3a3e2838a734d459b120e"
+};
+
+const app = initializeApp(firebaseConfig);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withInMemoryScrolling(scrollConfig)), provideHttpClient()]
+  providers: [
+    provideRouter(routes, withInMemoryScrolling(scrollConfig)),
+    provideHttpClient(),
+    importProvidersFrom([
+      provideFirestore(() => getFirestore())
+    ])
+  ]
 };
 
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../../shared/user.service";
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  public constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(
+      users => console.log(users)
+    )
+  }
 
 }
