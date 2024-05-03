d016be7033e8da4a43881ef921e6777bf67f5f5a
Jaiches22 <jaimecalzada22@gmail.com>
2024-05-03 11:14:18 +0100

    Finishing invoice-list

--- src/app/components/invoice-list/invoice-list.component.ts
+++ src/app/components/invoice-list/invoice-list.component.ts
@@ -9,6 +9,11 @@ import { TableModule } from 'primeng/table';
   styleUrl: './invoice-list.component.scss'
 })
 export class InvoiceListComponent {
-products: any;
+products = [
+  {comercio: "Amazon", concepto: "Ordenador nuevo", tipogasto: "Personal", fecha: "10-02-1999", precio: 100},
+  {comercio: "Zara", concepto: "Calcetines", tipogasto: "Ropa", fecha: "21-03-1873", precio: 30},
+  {comercio: "Mercadona", concepto: "Galletas", tipogasto: "Comida", fecha: "31-01-1345", precio: 5},
+];
+
 
 }
