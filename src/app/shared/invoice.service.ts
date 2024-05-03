a076e61b83e367f86345a2f2640826b52f8c5ed9
Pablo Santana <pablo.santana116@alu.ulpgc.es>
2024-05-03 19:09:39 +0100

    Created services

---
+++ src/app/shared/invoice.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {addDoc, collection, collectionData, deleteDoc, doc, Firestore, setDoc} from "@angular/fire/firestore";
+import {Observable} from "rxjs";
+import {Invoice} from "../models/invoice.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InvoiceService {
+
+  constructor(private firestore: Firestore) { }
+
+  getUserInvoices(id: string): Observable<Invoice[]>{
+    const invoicesRef = collection(this.firestore, `/users/${id}/invoices`);
+    return collectionData(invoicesRef, {idField: 'id'}) as Observable<Invoice[]>;
+  }
+
+  addInvoice(userId: string, invoice: Invoice){
+    const invoicesRef = collection(this.firestore, `/users/${userId}/invoices`);
+    return addDoc(invoicesRef, invoice);
+  }
+
+  updateUserInvoice(userId: string, invoice: Invoice): any{
+    const invoicesRef = doc(this.firestore, `/users/${userId}/invoices/${invoice.id}`);
+    return setDoc(invoicesRef, invoice);
+  }
+
+  deleteInvoice(userId: string, invoiceId: string) {
+    const invoicesRef = doc(this.firestore, `/users/${userId}/invoices/${invoiceId}`);
+    return deleteDoc(invoicesRef);
+  }
+}
--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {addDoc, collection, collectionData, Firestore} from "@angular/fire/firestore";
+import {addDoc, collection, collectionData, deleteDoc, doc, Firestore, setDoc} from "@angular/fire/firestore";
 import {User} from "../models/user.model";
 import {Observable} from "rxjs";
 
@@ -16,7 +16,17 @@ export class UserService {
   }
 
   getUsers(): Observable<User[]> {
-    const usersRef = collection(this.firestore, 'User');
+    const usersRef = collection(this.firestore, 'users');
     return collectionData(usersRef, {idField: 'id'}) as Observable<User[]>;
+  }
+
+  updateUser(user: User){
+    const userDocRef = doc(this.firestore, `users/${user.id}`);
+    return setDoc(userDocRef, user);
+  }
+
+  deleteUser(user: User){
+    const userDocRef = doc(this.firestore, `users/${user.id}`);
+    return deleteDoc(userDocRef);
   }
 }
